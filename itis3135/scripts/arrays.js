9a73f362981740d13bd62e4981a2aa2e39718a84
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-03-15 18:23:23 -0400

    update

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,10 +1,8 @@
-window.onclick = function()
-{
-    document.getElementById("display-results").onclick = displayResults;
-}
-
 const person = ["Rick Rambus", "Jenny Doe", "Stumpy Doe", "Luke Doe", "John Majors", "Papa Muntz", "Ted Muntz"];
 const salary = [3100, 2900, 2500, 2000, 20000, 9000, 5000];
+
+const resultsButton = document.getElementById("display-results") //testing
+        resultsButton.addEventListener('click', displayResults) //testing
 
 function addSalary()
 {
@@ -16,14 +14,14 @@ function displayResults()
     var sum = 0;
     var averageSalary;
 
-    //TODO get average salary
+    //Derive the average salary of array
     for(var i = 0; i < salary.length; i++)
     {
         sum += salary[i];
         averageSalary = sum / salary.length;
     }
     
-    //TODO get highest salary
+    //Derive the highest salary of array
     for(i = 0; i < salary.length; i++)
     {
         var currentSalary = salary[i];
@@ -34,8 +32,23 @@ function displayResults()
         }
     }
 
+    //highest_salary.innerHTML = "Test"; 
+
     //TODO display results (divide them) in the div element with "results" as the id
+    //add nodes to the DOM with the heading as an h2 element and the average and highest as <p> elements. 
+    //The easiest way to do that is to use the innerHTML property as shown in figure 6-13 in your Murach’s book.
     
+    //Event listener syntax (freeCodeCamp)
+    //element.addEventListener(event, function, booleanValue);
+
+    //element.addEventListener("click", function(){id.style.background = "Changes"});
+    //element.onClick = function(){id.style.background = "Changes"}); 
+
+    //resultsButton.addEventListener('click', function master(){ //testing
+        //var master = document.getElementsByClassName("master2"); //testing
+        //master[2].innerHTML = 'i need a job'; //testing
+    //}) //testing
+    console.log(highestSalary);
 }
 
 function displaySalary()
