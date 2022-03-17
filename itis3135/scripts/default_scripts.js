f0781b10e05e1d3e790c1b3dddcdce0b3ba1f673
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-03-17 10:40:10 -0400

    added dropdown button

--- itis3135/scripts/default_scripts.js
+++ itis3135/scripts/default_scripts.js
@@ -8,3 +8,23 @@ function scriptTest()
   alert("Script is Running");
 }
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdownFunction() {
+  document.getElementById("calculators-dropdown-options").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.calculators-dropdown-btn')) {
+    var dropdowns = document.getElementsByClassName("calculators-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
