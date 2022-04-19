e9b23f993921578164edc1c0cfc411d5948db932
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-04-19 17:43:20 -0400

    updates to slideshow

--- itis3135/scripts/slideshow_script.js
+++ itis3135/scripts/slideshow_script.js
@@ -1,35 +1,14 @@
-var $ = function(id) {return document.getElementById(id); };
+let slideIndex = 0;
+showSlides();
 
-window.onload = function() 
-{
-    var listNode = $("image_list");
-    var captionNode = $("caption");
-    var imageNode= $("image");
-
-    var link = listNode.getElementsByTagName("a");
-
-    //Process image links
-    var i, linkNode, image;
-    var imageCache = [];
-    for (i = 0; i < links.length; i++)
-    {
-        linkNode = links[i];
-        //Preload image and copy title properties
-        image = new Image();
-        image.src = linkNode.getArribute("href");
-        image.title = linkNode.getArribute("title");
-        imageCache[imageCache.length] = image;
-    }
-    
-    //Start slideshow
-    var imageCounter = 0;
-    var timer = setInterval(
-        function() 
-        {
-            imageCounter = (imageCounter+1) % imageCache.length;
-            image = imageCache[imageCounter];
-            imageNode.src = image.src;
-            captionNode.firstChild.nodeValue = image.title;
-        },
-        2000);
-};
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slideshow-slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
