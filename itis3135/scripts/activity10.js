ecb84dbe3281342dc5b30bd939a2f5c5b4737a9b
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-04-26 19:29:54 -0400

    summary

---
+++ itis3135/scripts/activity10.js
@@ -0,0 +1,28 @@
+$(document).ready(function()
+{
+    // preload the image for each link
+    $("#image_list a").each(function()
+    {
+        var swappedImage = new Image();
+        swappedImage.src = $(this).attr('href');
+    });
+
+    // set up the event handlers for each link
+    $("#image_list a").click(function(evt)
+    {
+        //swap image
+        var imageURL = $(this).attr("href");
+        $("#main_image").attr("src", imageURL);
+
+        //swap caption
+        var caption = $(this).attr("title");
+        $("#caption").text(caption);
+
+        //cancel the default action of the link
+        evt.preventDefault(); //jQuery cross-browser method
+    }); //end click
+
+    //move focus to first thumbnail
+    $("li:first-child a").focus();
+
+}); //end ready
