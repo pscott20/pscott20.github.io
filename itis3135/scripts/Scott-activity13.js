f022bb9ef81af578d2441e39baf91c93d7748e24
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-05-04 17:29:45 -0400

    updated activity13 files

---
+++ itis3135/scripts/Scott-activity13.js
@@ -0,0 +1,12 @@
+$.getJSON("team.json", function(data)
+{
+    $.each(data, function() {
+        $.each(this, function(key, value) {
+            $("#team").append(
+                "Name: " + value.name + "<br>" +
+                "Title: " + value.title + "<br>" +
+                "Bio: " + value.bio + "<br><br>"
+            );
+        });
+    });
+}); 
