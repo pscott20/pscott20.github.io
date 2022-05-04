29bc9436675723fd2f02d86428918074bf9918a6
pscott20 <89850257+pscott20@users.noreply.github.com>
2022-05-04 16:28:55 -0400

    activity 12
    updated

--- itis3135/scripts/Scott-Activity12.js
+++ itis3135/scripts/Scott-Activity12.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
     $("#slider").bxSlider({
         auto: true,
-        minSlides: 2,
-        maxSlides: 2,
-        slideWidth: 300,
-        slideMargin: 20
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20,
+        randomStart: true,
+        caption: true,
+        speed: 3000,
+        pager: true,
+        
     });
 });
